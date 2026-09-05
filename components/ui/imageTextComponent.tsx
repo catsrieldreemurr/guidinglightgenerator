b8b4cd1415fa881df3d3241b51f66cef6cc27bd3
Vultus Limunaria <danobrovold@gmail.com>
2026-09-06 01:45:27 +0200

    Fix ImageTextComponent

--- components/ui/imageTextComponent.tsx
+++ components/ui/imageTextComponent.tsx
@@ -12,18 +12,21 @@ interface ImageTextComponentProps{
 
 export default function ImageTextComponent({ imageDirection, imageURL, imageAlt, headerText, children }:ImageTextComponentProps){
     return ( 
-        <div className={`flex gap-5 flex-col 
+        <div className={`flex gap-5 flex-col justify-center items-center
             ${(imageDirection === "imageRight" || imageDirection === undefined) && 'sm:flex-row'} 
             ${imageDirection === "imageLeft" && 'sm:flex-row-reverse'} gap-5`}>
-
-            <div className="flex flex-col items-center justify-center border-2 border-blue-300">
+            
+            <div className="flex items-center">
+                <div className="flex flex-col items-center justify-center border-2 border-blue-300 p-5">
                 <Typography isBold>{headerText}</Typography>
                 <div className="flex gap-5">
                    {children}
                 </div>
                 
+                </div>
             </div>
-            <Image src={imageURL} alt={imageAlt} height={200} width={200}/>
+            
+            <Image src={imageURL} alt={imageAlt} className="rounded-xl" height={200} width={200}/>
         </div>
     )
 }
